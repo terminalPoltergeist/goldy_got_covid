776b4736411669c945a68f85b7a3c902319fe747
terminalPoltergeist <nemit031@umn.edu>
2021-01-24 11:08:51 -0600

    JSON parsing

--- scraper.py
+++ scraper.py
@@ -1,9 +1,26 @@
 import requests
 # module for api GET requests
+import json
+# module for handling JSON data from APIs
 
+# current mn covid data (all JSON data from the state)
 mn_current = requests.get(
-    "https://api.covidtracking.com/v1/states/mn/current.json")
-mn_historic = requests.get(
-    "https://api.covidtracking.com/v1/states/mn/daily.json")
+    "https://api.covidtracking.com/v1/states/mn/current.json?date")
+# historical mn covid data (all JSON data from the state)
+# mn_historic = requests.get(
+#    "https://api.covidtracking.com/v1/states/mn/daily.json")
 
-print(mn_historic.json())
+mn_current_dict = json.loads(mn_current.text)
+print(mn_current_dict["date"])
+
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+#print('current data')
+# jprint(mn_current.json())
+#print('historical data')
+# jprint(mn_historic.json())
