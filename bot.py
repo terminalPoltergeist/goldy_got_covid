d1f6889264a83479176ed1ee9c86b5677709e3d8
terminalPoltergeist <nemit031@umn.edu>
2021-01-24 19:23:40 -0600

    added api object, waiting to get Twitter dev account access

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import tweepy
-import api_keys
+import keys
+
+api_key = keys.api_key
+api_secret = keys.api_secret
+access_token = keys.access_token
+access_token_secret = keys.access_token_secret
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("api_key", "api_secret")
