9466517e54e7087b060aa64c691b4cb12b55f12b
terminalPoltergeist <nemit031@umn.edu>
2021-01-24 22:46:47 -0600

    created JSON data starage

--- scraper.py
+++ scraper.py
@@ -7,6 +7,9 @@ import requests
 import json
 # module for handling JSON data from APIs
 from datetime import date, timedelta
+# date and time madule
+from bs4 import BeautifulSoup
+# web scraper library
 
 # current mn covid data (all JSON data from the state)
 mn_current = requests.get(
@@ -30,10 +33,10 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-
 # prints JSON formatted data
-jprint(mn_current.json())
+#/ jprint(mn_current.json())
 #/ jprint(mn_historic.json())
+
 
 # gets today's date in form YYYY-MM-DD, conversts to str
 dt = str(date.today())
@@ -42,3 +45,35 @@ today = int(dt.replace("-", ""))
 # calculates yesterday's date and formats it as MM/DD/YY
 yesterday = date.today() - timedelta(days=1)
 yesterday = yesterday.strftime('%m/%d/%y')
+
+URL = "https://safe-campus.umn.edu/return-campus/covid-19-dashboard"
+dashboard = requests.get(URL).text
+# data = html.fromstring(dashboard.content)
+# num = data.xpath(
+#    '//*[@id="block-folwell-content"]/article/div/div[1]/div/div[3]/div/div[2]/div/div/div[2]/div[2]/div[2]/div[2]/div[1]')
+soup = BeautifulSoup(dashboard, "lxml")
+cases = soup.findAll(
+    "div", class_="field field--name-field-number-of-cases field--type-float field--label-hidden field__item")
+U_cases_week = int(cases[7].string.replace(",", ""))
+U_cases_total = int(cases[6].string.replace(",", ""))
+
+percentages = soup.findAll(
+    "div", class_="field field--name-field-percentage-positive-test field--type-float field--label-hidden field__item")
+percent_total = float(percentages[0].string)
+percent_week = float(percentages[1].string)
+
+
+data = {}
+data['mn_cases_today'] = cases_today
+data['mn_cases_total'] = cases_total
+data['mn_deaths_today'] = deaths_today
+data['mn_deaths_total'] = deaths_total
+data['ucases_week'] = U_cases_week
+data['ucases_total'] = U_cases_total
+data['total_percent'] = percent_total
+data['week_percent'] = percent_week
+
+print(data)
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
